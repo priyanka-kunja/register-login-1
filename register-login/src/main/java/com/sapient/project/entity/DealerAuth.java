395f30438d63c028b8f81118b7927c3a9cfb155c
Pothumudi Sireesha <pothumudi.sireesha@publicissapient.com>
2020-10-06 22:55:00 +0530

    Initial Commit

---
+++ register-login/src/main/java/com/sapient/project/entity/DealerAuth.java
@@ -0,0 +1,29 @@
+package com.sapient.project.entity;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "Dealer_auth")
+public class DealerAuth {
+	@Id
+	@Column(name = "dealer_userid")
+    private String userId;
+	@Column(unique=true,nullable=false,name = "dealer_id")
+	private Long dealerId;
+	@Column(name = "dealer_password")
+	private String password;
+	@Column(name = "dealer_mail")
+	private String mailId;
+	@Column(name = "dealer_phn_no")
+	private Long phoneNumber;
+}
